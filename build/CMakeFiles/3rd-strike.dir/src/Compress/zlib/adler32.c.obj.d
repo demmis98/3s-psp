692c721ed24a754b90b2b734bfd498d1bbbb4a8f
3sxtra <3sxtra@users.noreply.github.com>
2026-03-19 16:40:27 +0000

    chore: stop tracking build/ directory

--- build/CMakeFiles/3rd-strike.dir/src/Compress/zlib/adler32.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/3rd-strike.dir/src/Compress/zlib/adler32.c.obj: \
- /home/demma/Documents/code/psp/3rd-strike/src/Compress/zlib/adler32.c \
- /home/demma/Documents/code/psp/3rd-strike/include/Compress/zlib/zlib.h \
- /home/demma/Documents/code/psp/3rd-strike/include/Compress/zlib/zconf.h
